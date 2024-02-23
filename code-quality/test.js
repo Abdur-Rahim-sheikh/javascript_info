c9a3b35e6b2087dde0ef73e0380b5ed8b9d800b1
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2024-02-23 15:36:49 +0600

    added more test

--- code-quality/test.js
+++ code-quality/test.js
@@ -1,11 +1,27 @@
 describe("power", function() {
-
-    it("2 raises to power 3 is 8", function() {
-      assert.equal(pow(2, 3), 8);
+    describe("raises x to power 3", function(){
+        function makeTest(x){
+            let expected = x**3;
+            it(`${x} in the power 3 is ${expected}`, function(){
+                assert.equal(pow(x,3), expected);
+            });
+        }
+    
+        for(let x=1; x <= 5; x++){
+            makeTest(x);
+        }
+    });
+    
+    it("for negative n the result is NaN", function() {
+        assert.isNaN(pow(2, -1));
     });
 
-    it("3 raises to power 4 is 81", function(){
-        assert.equal(pow(3,4), 81);
+    it("for non-integer n the result is NaN", function() {
+        assert.isNaN(pow(2, 1.5));
+    });
+
+    it("for non-integer n the result is NaN", function() {
+        assert.isNaN(pow(2, 2.5));
     });
   
   });
