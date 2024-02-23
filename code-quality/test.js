1c501128f94d864b9697fc2225e0de0d23d1e3e5
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2024-02-23 14:57:05 +0600

    manual check done

---
+++ code-quality/test.js
@@ -0,0 +1,11 @@
+describe("power", function() {
+
+    it("2 raises to power 3 is 8", function() {
+      assert.equal(pow(2, 3), 8);
+    });
+
+    it("3 raises to power 4 is 81", function(){
+        assert.equal(pow(3,4), 81);
+    });
+  
+  });
